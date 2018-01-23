fed497f8f7fa14eeeba2e70bcd312be55d0ae65c
Kelvin Li <jpkelvinli@gmail.com>
2018-01-22 17:13:36 -0800

    Separate context menu from i2g. Integrate i2g dependencies (package.json) into COVA package.json.

--- i2g/i2g.js
+++ i2g/i2g.js
@@ -1,6 +1,7 @@
 define(function(require) {
     var logos = require('./icons'),
-        download = require('./downloadFunc'); 
+        download = require('./downloadFunc'),
+        menu = require('./menu');
 
     return function(arg) {
         'use strict';
@@ -12,8 +13,6 @@ define(function(require) {
             graph = options.graph || {},
             width = options.width || 600,
             height = options.height || 600,
-            menu = options.menu || false,
-            notePanel = options.notePanel || null,
             onselect = options.onselect || function() {},
             graphId = options.graphId || 'igraph-svg',
             graphName = options.graphName || '',
@@ -29,19 +28,27 @@ define(function(require) {
         var maxLinkValue = 0,
             nodeCounter = 0;
 
+
+        /** Use a temp link to show the link when add a new link */
+        var tempLink = {
+            source: null,
+            targe: null,
+            svg: null
+        }
+
         // set up the svg
         var svg = d3.select(container).append('svg:svg');
         svg.attr('id', graphId)
         .attr("width", width)
         .attr("height", height)
         .style("border", "solid 3px black")
-        .on('click', function(d){   
+        .on('click', function(d){
             // modify the templink
-            if(linkSource !== null) {
-                //reset linkSource
-                linkSource = null;
-                
-                tempLink
+            if(tempLink.source !== null) {
+                //reset tempLink.source
+                tempLink.source = null;
+
+                tempLink.svg
                     .attr('stroke-width', 0)
                     .attr('x1', 0)
                     .attr('y1', 0)
@@ -115,13 +122,7 @@ define(function(require) {
             .stop();
 
 
-
-        /** Use a temp link to show the link when add a new link */
-        var linkSource = null,
-            linkTarget = null,
-            newArrivingNode = null;
-
-        var tempLink = svg.append('g')
+        tempLink.svg = svg.append('g')
             .append('line')
             .attr('id', 'tempLink')
             .attr('stroke', '#CCC')
@@ -134,9 +135,12 @@ define(function(require) {
 
         // change the destination of the temp link when move the mouse
         svg.on('mousemove', function(e){
-            if(linkSource !== null){
+
+            if(tempLink.source !== null){
                 var pos = d3.mouse(this);
-                tempLink.attr('x2', pos[0]-3)
+
+                tempLink.svg
+                    .attr('x2', pos[0]-3)
                     .attr('y2', pos[1]-3);
             }
         });
@@ -171,7 +175,7 @@ define(function(require) {
         //=====================================================================
         // Private Functions
         //=====================================================================
-        
+
         /** Set up a restart function to plot all the nodes and links. */
         function restart() {
 
@@ -197,26 +201,26 @@ define(function(require) {
                 .attr("fill", "white")
                 .attr("r", 30)
                 .merge(node)
-                .on('click', function(d){   
+                .on('click', function(d){
                     // d.fx = d3.mouse(this)[0];
                     // d.fy = d3.mouse(this)[1];
                 // console.log('clicked on node', d.id);
 
                 // modify the templink
-                if(linkSource !== null && linkTarget === null) {
-                        linkTarget = d;
-                        // console.log(linkTarget);
+                if(tempLink.source !== null && tempLink.target === null) {
+                        tempLink.target = d;
+                        // console.log(tempLink.target);
                         i2g.addLinks({
-                            source: linkSource,
-                            target: linkTarget,
+                            source: tempLink.source,
+                            target: tempLink.target,
                             value: 2,
                             datalink: false
                         })
 
                         // set temp link to original state
-                        linkSource = linkTarget = null;
-                        
-                        tempLink
+                        tempLink.source = tempLink.target = null;
+
+                        tempLink.svg
                             .attr('stroke-width', 0)
                             .attr('x1', 0)
                             .attr('y1', 0)
@@ -232,12 +236,12 @@ define(function(require) {
                     .on("end", dragended)
                 );
 
-            // Unknown action 
+            // Unknown action
             node
             .append('title')
             .text((d)=>{ if(d.detail) return d.detail});
             // console.log(node);
-            
+
             // For each node add icon and labels
             node.data(nodes, function(d){
                 addNodeIcon(d);
@@ -423,234 +427,14 @@ define(function(require) {
 
 
 
-        /** Set up a group of context menu functions. */
-        function svgMenu() {
-            $.contextMenu.types.label = function(item, opt, root) {
-                // this === item.$node
-                $('<div>Please input a file<div>\
-                    <input type="file" id="myFile" multiple size="50">\
-                    <div id="demo"></div>')
-                    .appendTo(this)
-                    .on('change', function() {
-                        var x = document.getElementById("myFile");
-                        var txt = "";
-                        if ('files' in x) {
-                            if (x.files.length == 0) {
-                                txt = "Select one or more files.";
-                            } else {
-                                for (var i = 0; i < x.files.length; i++) {
-                                    txt += "<br><strong>" + (i+1) + ". file</strong><br>";
-                                    var file = x.files[i];
-                                    if ('name' in file) {
-                                        txt += "name: " + file.name + "<br>";
-                                    }
-                                    if ('size' in file) {
-                                        txt += "size: " + file.size + " bytes <br>";
-                                    }
-                                }
-                            }
-                        } 
-                        else {
-                            if (x.value == "") {
-                                txt += "Select one or more files.";
-                            } else {
-                                txt += "The files property is not supported by your browser!";
-                                txt  += "<br>The path of the selected file: " + x.value; // If the browser does not support the files property, it will return the path of the selected file instead. 
-                            }
-                        }
-                        document.getElementById("demo").innerHTML = txt;
-                    });
-            };
 
-            $.contextMenu({
-                selector: container, 
-                callback: function(key, options) {
-                    var newNodeType;
-                    var newNodePosition = $(".context-menu-root").first().position();
-                    if(key == 'uploadFile') {
-                        var x = document.getElementById("myFile").files[0];
-                        var reader = new FileReader();
-                        reader.onload = function(){
-                            var graphData = JSON.parse(reader.result);
-                            nodes.forEach((d) => {
-                                d3.select(d).remove();
-                                removeNode(d.id);
-                                restart();
-                            });
-                           	nodeCounter = 0;
-                            var uploadNodes = graphData.nodes;
-                            uploadNodes.forEach((d) => {
-                                i2g.addNodes({
-						            label: d.label,
-						            type: d.type,
-						            fx: d.fx,
-						            fy: d.fy,
-						            value: d.value,
-						            datalink: d.datalink
-						        }).update();
-                            });
-                            var uploadLinks = graphData.links;
-                            uploadLinks.forEach((d) => {
-                                i2g.addLinks({
-						            source: d.source.id,
-						            target: d.target.id,
-						            value: d.value
-						        }).update();
-                            });
-                        };
-                        reader.readAsText(x);
-                    } else if(key == 'saveFile') {
-                        var graphData = ({
-                            nodes: nodes,
-                            links: links
-                        });
-                        var fileName = options.inputs["downloadFileName"].$input.val();
-                        download(graphData, fileName);
-                    } else {
-                        if(key == 'location') {
-                            newNodeType = 'location';
-                        } else if(key == 'people') {
-                            newNodeType = 'people';
-                        } else if(key == 'time') {
-                            newNodeType = 'time';
-                        }
-                        i2g.addNodes({
-                            label: 'New ' + key,
-                            type: newNodeType,
-                            fx: newNodePosition.left,
-                            fy: newNodePosition.top,
-                            value: 0,
-                            datalink: false
-                        }).update();
-                    }
-                },
-                items: {
-                    addNode: {
-                        name: "Add node",
-                        icon: "fa-plus-square",
-                        items: {
-                            location: {
-                                name: "Location",
-                                icon: "fa-map-marker"
-                            },
-                            people: {
-                                name: "People",
-                                icon: "fa-user"
-                            },
-                            time: {
-                                name: "Time",
-                                icon: "fa-clock-o"
-                            }
-                        }
-                    },
-                    importGraph: {
-                        name: "Import graph",
-                        icon: "fa-upload",
-                        items: {
-                            uploadFileName: {
-                                name: "Please input a file",
-                                type: 'label',
-                                callback: function(){ return false; }
-                            },
-                            sep1: "---------",
-                            uploadFile: {
-                                name: "upload file",
-                                icon: "fa-folder-open"
-                            }
-                        }
-                    },
-                    exportGraph: {
-                        name: "Export graph",
-                        icon: "fa-download",
-                        items: {
-                            downloadFileName: {
-                                name: "Please input the file name",
-                                type: 'text', 
-                                value: ""
-                            },
-                            sep1: "---------",
-                            saveFile: {
-                                name: "Save file",
-                                icon: "fa-floppy-o"
-                            }
-                        }
-                    },
-                }
-            });
-        }
-
-        function nodeMenu() {
-            $.contextMenu({
-                selector: '.nodeHolder',
-                callback: function(key, options) {
-                    var thisNode = this[0],
-                        thisNodeId = thisNode.__data__.id;
-                    if(key == 'removeNode') {
-                        d3.select(thisNode).remove();
-                        removeNode(thisNodeId);
-                        restart();
-                    } else if(key == 'annotate') {
-                        d3.select(thisNode).attr('stroke', 'orange');
-                        // console.log(nodeHash[thisNodeId]);
-                        notePanel.setNote({
-                            label: nodeHash[thisNodeId].labelPrefix+nodeHash[thisNodeId].label,
-                            detail: nodeHash[thisNodeId].detail
-                        })
-                        notePanel.show();
-                        notePanel.onsave = function() {
-                            var info = notePanel.getNote();
-                            info.labelPrefix = '';
-                            i2g.modifyNode(thisNodeId, info);
-                            d3.select(thisNode).attr('stroke', 'transparent');
-                        }
-                        notePanel.oncancel = function() {
-                            d3.select(thisNode).attr('stroke', 'transparent');
-                        }
-                    } else if(key == 'addLink'){
-                        linkSource = nodeHash[thisNodeId];
-                        linkTarget = null;
-                        tempLink
-                            .attr('x1', linkSource.x)
-                            .attr('y1', linkSource.y)
-                            .attr('x2', linkSource.x)
-                            .attr('y2', linkSource.y)
-                            .attr('stroke-width', 4);
-                    }
-                },
-                items: {
-                    removeNode: {name: "Remove this node", icon: "fa-times"},
-                    addLink: {name: "Add link", icon: "fa-long-arrow-right"},
-                    annotate: {name: "Annotate", icon: "fa-commenting"},
-                }
-            });
-        }
-
-        function linkMenu() {
-            $.contextMenu({
-                selector: '.graphLinks',
-                callback: function(key, options) {
-                    var thisLink = this[0],
-                        thisLinkId = thisLink.__data__.id;
-                    if(key == 'removeLink') {
-                        d3.select(thisLink).remove();
-                        removeLink(thisLinkId);
-                    } else if(key == 'annotate') {
-
-                    }
-                },
-                items: {
-                    removeLink: {name: "Remove this link", icon: "fa-times"},
-                    annotate: {name: "Annotate", icon: "fa-commenting"},
-                }
-            });
-        }
         //=====================================================================
         //End of Private Functions
         //=====================================================================
 
-        svgMenu();
-        nodeMenu();
-        linkMenu();
+        menu.svgMenu(container, i2g);
+        menu.nodeMenu(i2g, tempLink);
+        menu.linkMenu(i2g);
 
 
         //=====================================================================
@@ -834,6 +618,8 @@ define(function(require) {
             restart();
             return i2g;
         }
+
+        i2g.nodeHash = nodeHash;
 
         i2g.getNodes = function(query) {
             if(typeof query === 'undefined')
